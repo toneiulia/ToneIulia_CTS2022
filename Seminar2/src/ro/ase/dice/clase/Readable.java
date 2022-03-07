4b5c15b2c19f14973c214596ed3a40e5d7a3aece
toneiulia <91604412+toneiulia@users.noreply.github.com>
2022-03-07 15:39:48 +0200

    Add files via upload

---
+++ Seminar2/src/ro/ase/dice/clase/Readable.java
@@ -0,0 +1,29 @@
+package ro.ase.dice.clase;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public abstract class Readable {
+    protected Scanner scanner;
+
+    public Readable(String file) throws FileNotFoundException {
+        scanner = new Scanner(new File(file));
+    }
+
+    public abstract List<Aplicant> readAplicants() throws FileNotFoundException;
+
+    public void readAplicant(Scanner scanner, Aplicant aplicant) {
+        aplicant.setNume(scanner.next());
+        aplicant.setPrenume(scanner.next());
+        aplicant.setVarsta(scanner.nextInt());
+        aplicant.setPunctaj(scanner.nextInt());
+
+        int nrProiecte = scanner.nextInt();
+        String[] proiecte = new String[nrProiecte];
+        for (int i = 0; i < nrProiecte; i++)
+            proiecte[i] = scanner.next();
+        aplicant.setNr_proiecte(nrProiecte, proiecte);
+    }
+}
